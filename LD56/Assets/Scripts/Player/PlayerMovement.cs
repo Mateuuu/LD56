5307f2170b78958edff13d878b712b06d422c505
Mateuuu <matthewal194@gmail.com>
2024-10-04 20:15:23 -0700

    Making the scene

---
+++ LD56/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float acceleration = 50f;
+    [SerializeField] LayerMask floor;
+
+    Rigidbody rb;
+    Vector2 movement;
+
+    Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Vector3 hitPos = Vector3.zero;
+
+        if(Physics.Raycast(ray, out hit, 100)){
+
+            hitPos = ray.origin;
+
+        }
+
+        transform.LookAt(hitPos);
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // movement.x = Mathf.Pow(movement.x, 5);          // Make sure this is an odd power so that the sign stays consistent
+        // movement.y = Mathf.Pow(movement.y, 5);
+    }
+
+
+    private void FixedUpdate()
+    {
+        float moveMultiplier = 1f;
+
+        if(movement.x != 0 && movement.y != 0)
+        {
+            moveMultiplier *= .7071f;           // Sqrt(2) / 2
+        }
+
+
+        float xMovement = movement.x * moveSpeed * moveMultiplier;
+        float yMovement = movement.y * moveSpeed * moveMultiplier;
+
+        Vector3 targetVelocity = new Vector3(xMovement, 0, yMovement);
+
+        rb.velocity = new Vector3(xMovement, 0, yMovement);
+
+        rb.velocity = Vector3.MoveTowards(rb.velocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+    }
+
+}
