ff7a399106c6253b404fd1fa37f9c9ad9cd14c88
Mateuuu <matthewal194@gmail.com>
2024-10-05 22:49:42 -0700

    Hallway mostly

--- LD56/Assets/Scripts/ExitLevel.cs
+++ LD56/Assets/Scripts/ExitLevel.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour
 {
-    
+
+    [SerializeField]
+    [SceneAttribute] string sceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerBody"))
+        {
+
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+    }
 
 }
