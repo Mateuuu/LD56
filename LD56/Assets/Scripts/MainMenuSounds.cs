92f1795778d4c04caee0605e3d13a0e25c73e01b
Mateuuu <matthewal194@gmail.com>
2024-10-06 22:47:54 -0700

    Animation and lots of other stuff I guess

--- LD56/Assets/Scripts/MainMenuSounds.cs
+++ LD56/Assets/Scripts/MainMenuSounds.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MainMenuSounds : MonoBehaviour
 {
-    void Start()
+    IEnumerator Start()
     {
         AudioManager.instance.StopAllSongs();
         AudioManager.instance.StartSong("MainMenu");
+
+
+        yield return new WaitForSeconds(.25f);
+        AudioManager.instance.PlaySound("VanEnter");
+
+        yield return new WaitForSeconds(6.6f);
+        AudioManager.instance.PlaySound("VanIdle");
+
     }
 }
--- LD56/Assets/Scripts/UI/MainMenuButtons.cs
+++ LD56/Assets/Scripts/UI/MainMenuButtons.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class MainMenuButtons : MonoBehaviour
 {
 
+    [SerializeField] Animator vanAnimator;
+
     [SerializeField] SceneTransition sceneTransition;
     public void Play()
     {
+        AudioManager.instance.PlaySound("ButtonClick");
+
+        StartCoroutine(PlayTransition());
+
+    }
+
+    IEnumerator PlayTransition()
+    {
+
+        vanAnimator.SetBool("DriveAway", true);
+        AudioManager.instance.PlaySound("VanDrive");
+
+
+        yield return new WaitForSeconds(1f);
 
         // Do a coroutine here that makes it so that the car drives away with some sounds =D
 
         sceneTransition.TransitionToScene("Intro Scene");
     }
 
+
     public void Quit()
     {
+        AudioManager.instance.PlaySound("ButtonClick");
 
         Application.Quit();
     }
